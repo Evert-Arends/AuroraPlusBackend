e5a58adf509b3660eec331a9481cd2950dafeeff
Evert Arends <evert-arends@hotmail.nl>
2016-11-05 22:49:20 +0100

    Lekker git pushen :3

---
+++ AuroraPlusBack/settings/constants.py
@@ -0,0 +1,9 @@
+# Define server variables here.
+
+API_SERVER_BASE = '127.0.0.1'
+API_SERVER_PORT = '8001'
+
+# Do not touch zone.
+API_SERVER_ADDRESS = '{0}:{2}'.format(API_SERVER_BASE, ':', API_SERVER_PORT)
+
+
